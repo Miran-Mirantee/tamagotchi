5cebeeaa3268b63d0461235054107913c50c8ecc
Miran-Mirantee <killerm11805@hotmail.com>
2025-01-29 02:04:54 +0700

    added cat and tested animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Experience from "./Experience";
 export default function App() {
   return (
     <>
-      <Canvas>
+      <Canvas camera={{ position: [0, 0.1, 3], fov: 45, near: 0.1, far: 200 }}>
+        <color args={["rgba(62,58,74,1)"]} attach={"background"} />
         <Experience />
       </Canvas>
     </>
--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,15 +1,14 @@
-import { Stage, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+import { Cat } from "./components/Cat";
 
 export default function Experience() {
   return (
     <>
       <OrbitControls makeDefault />
-      <Stage>
-        <mesh>
-          <boxGeometry />
-          <meshNormalMaterial />
-        </mesh>
-      </Stage>
+      <Cat scale={0.5} />
+      <ambientLight />
+      <directionalLight intensity={2.5} position={[3, 6, 3]} />
+      <directionalLight intensity={1} position={[-3, 2, -6]} />
     </>
   );
 }
