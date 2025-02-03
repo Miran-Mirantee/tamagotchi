0ffaca6a8908368dcc5d5154008c95e86ff0e4c6
Miran-Mirantee <killerm11805@hotmail.com>
2025-02-03 21:36:11 +0700

    added todos list

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,23 @@ import { Perf } from "r3f-perf";
 import Experience from "./Experience";
 import UI from "./components/UI";
 
+/**
+ * TODO:
+ * 1. add more types of food
+ * 2. add more pet models to choose
+ * 3. add model customization feature
+ * 4. save data in localStorage
+ * 5. improve eat system
+ * 6. add more interactions with pet (petting, fetch a ball)
+ * 7. add activities to do with pet
+ * 8. add more needs
+ * 9. improve outside of tamagotchi scene
+ * 10. add tamagotchi model
+ * 11. add tamagotchi customization feature
+ * 12. improve inside of tamagotchi scene
+ * 13. (optional) add house customization feature
+ */
+
 export default function App() {
   return (
     <>
