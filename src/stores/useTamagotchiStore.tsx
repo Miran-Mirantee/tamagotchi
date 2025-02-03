017fec198ba3e0f1aa277529e227d1dcb5ec9792
Miran-Mirantee <killerm11805@hotmail.com>
2025-02-03 19:11:52 +0700

    added maximum value to needs

--- src/stores/useTamagotchiStore.tsx
+++ src/stores/useTamagotchiStore.tsx
@@ -12,6 +12,9 @@ type TamagotchiState = {
   hunger: number;
   energy: number;
   happiness: number;
+  maxHunger: number;
+  maxEnergy: number;
+  maxHappiness: number;
   isEating: boolean;
   currentFood: Food | null;
   setIsEating: (isEating: boolean) => void;
@@ -21,8 +24,11 @@ type TamagotchiState = {
 
 const useTamagotchiStore = create<TamagotchiState>((set) => ({
   hunger: 0,
-  energy: 100,
-  happiness: 100,
+  energy: 0,
+  happiness: 0,
+  maxHunger: 100,
+  maxEnergy: 100,
+  maxHappiness: 100,
   isEating: false,
   currentFood: null,
   setIsEating: (isEating) => set(() => ({ isEating })),
@@ -30,9 +36,18 @@ const useTamagotchiStore = create<TamagotchiState>((set) => ({
     set((state) => {
       if (!state.isEating && state.currentFood) {
         return {
-          hunger: state.hunger + state.currentFood.hunger,
-          energy: state.energy + state.currentFood.energy,
-          happiness: state.happiness + state.currentFood.happiness,
+          hunger: Math.min(
+            state.hunger + state.currentFood.hunger,
+            state.maxHunger
+          ),
+          energy: Math.min(
+            state.energy + state.currentFood.energy,
+            state.maxEnergy
+          ),
+          happiness: Math.min(
+            state.happiness + state.currentFood.happiness,
+            state.maxHappiness
+          ),
           isEating: true,
         };
       }
